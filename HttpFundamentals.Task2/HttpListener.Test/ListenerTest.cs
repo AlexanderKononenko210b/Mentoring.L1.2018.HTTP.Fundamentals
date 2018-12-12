e3857748ae363f13e6229cca84b88438aa8c2692
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-12-12 17:46:58 +0300

    Change OrderId => CustomerId according task.

--- HttpFundamentals.Task2/HttpListener.Test/ListenerTest.cs
+++ HttpFundamentals.Task2/HttpListener.Test/ListenerTest.cs
@@ -1,4 +1,9 @@
-﻿using HttpListener.BusinessLayer;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HttpListener.BusinessLayer;
 using HttpListener.BusinessLayer.Converters;
 using HttpListener.BusinessLayer.Parsers;
 using HttpListener.DataLayer;
@@ -9,6 +14,20 @@ namespace HttpListener.Test
     [TestFixture]
     public class ListenerTest
     {
+        private ListenerService _listenerService;
+
+        [SetUp]
+        public void Initialize()
+        {
+            using (var context = new NorthwindContext())
+            {
+                var parser = new Parser();
+                var orderRepository = new OrderRepository(context);
+                var converter = new Converter();
+                _listenerService = new ListenerService(parser, orderRepository, converter);
+            }
+        }
+
         [Test]
         public void Listener_Test()
         {
@@ -17,10 +36,26 @@ namespace HttpListener.Test
                 var parser = new Parser();
                 var orderRepository = new OrderRepository(context);
                 var converter = new Converter();
-                var listenerService = new ListenerService(parser, orderRepository, converter);
+                _listenerService = new ListenerService(parser, orderRepository, converter);
 
-                listenerService.Listen();
+                _listenerService.Listen();
             }
+
+            //Thread listenerThread = new Thread(_listenerService.Listen);
+            //listenerThread.Start();
+
+            //using (var client = new HttpClient())
+            //{
+            //    UriBuilder builder = new UriBuilder("http://localhost:81");
+            //    builder.Query = "customerId=10248";
+
+            //    var result = client.GetAsync(builder.Uri).Result;
+
+            //    using (StreamReader sr = new StreamReader(result.Content.ReadAsStreamAsync().Result))
+            //    {
+            //        Console.WriteLine(sr.ReadToEnd());
+            //    }
+            //}
         }
     }
 }
